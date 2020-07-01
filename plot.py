50b14ffc18817f4148f6d984de4d17cb7eca28e3
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2020-07-01 08:29:21 -0700

    Rename these variables, and print fraction
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- plot.py
+++ plot.py
@@ -33,19 +33,20 @@ def RussetPlot(image):
 
         # Need to map to tuple of range(0, 1)
         cn = normalize_color(vb)
+        color_label = '{}'.format(vb['color'])
+        pixel_count = vb['vbox'].count
+        pixel_percent = (pixel_count / total_pixels) * 100
 
         r1 = mpatch.Rectangle((0, i), 1, 1, color=cn)
         r2 = mpatch.Rectangle((1, i), 1, 1, color=cn)
-        txt = ax.text(2, i+.5, '  ' + "hello", va='center', fontsize=10,
-                      weight=weight)
+        txt = ax.text(2, i+.5, '{} / {}'.format(pixel_count, total_pixels),
+                      va='center', fontsize=10, weight=weight)
         ax.add_patch(r1)
         ax.add_patch(r2)
         ax.axhline(i, color='k')
 
-        color_label = '{}'.format(vb['color'])
-        pixel_count = (vb['vbox'].count / total_pixels) * 100
 
-        ax.text(.5, i + 1.5, '{0:.2f} %'.format(pixel_count), ha='center', va='center')
+        ax.text(.5, i + 1.5, '{0:.2f} %'.format(pixel_percent), ha='center', va='center')
         ax.text(1.5, i + 1.5, color_label, ha='center', va='center')
         ax.set_xlim(0, 3)
         ax.set_ylim(0, i + 2)
