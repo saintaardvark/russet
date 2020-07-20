632ccc30ac5fcd9e44e1ce3a58d26988d571b448
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2020-07-20 08:42:52 -0700

    Extend capture script a bit
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- capture.sh
+++ capture.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin
 
-mkdir data
-raspistill -e png -o data/"russet-$(date +'%s').png"
+LOCAL_DATADIR=/home/pi/data
+REMOTE_DATADIR=/backup/picam
+IMGFILE="${LOCAL_DATADIR}/russet-$(date +'%s').png"
+
+mkdir $LOCAL_DATADIR
+raspistill -e png -o $IMGFILE
+scp ${LOCAL_DATADIR}/*png rh:${REMOTE_DATADIR} && \
+	find ${LOCAL_DATADIR}/ -type f -name \*png -delete
